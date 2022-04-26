01e2ee559d319261aaf26cce9a5aa25b85912036
김혜인 <fallingstars2749@gmail.com>
2022-04-26 15:01:24 +0900

    :lipstick: [S06P31C105-80] Feat: Add Tab Navigation Icons
    탭 네비게이션에 아이콘을 추가하였음

--- frontend/src/navigations/Tab.tsx
+++ frontend/src/navigations/Tab.tsx
@@ -1,26 +1,104 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Camera, Bookmark, Diary } from "../screens";
+import { Search, Camera, Bookmark, Diary } from "../screens";
 import MainNavigator from "./MainNavigator";
-import { MaterialIcons } from '@expo/vector-icons';
+import { AntDesign } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 
 // Screen에 넘길 파라미터가 없으면 undefined, 있다면 파라미터 객체를 작성해 주세요!
 export type TabParamList = {
-    MainNavigator: undefined,
-    Camera: undefined,
-    Bookmark: undefined,
-    Diary: undefined,
-}
+  MainNavigator: undefined;
+  Search: undefined;
+  Camera: undefined;
+  Bookmark: undefined;
+  Diary: undefined;
+};
+
+type TabBarIconProp = {
+  focused: boolean;
+  color: string;
+  size: number;
+};
 
 const Tab = createBottomTabNavigator<TabParamList>();
 
 const TabNav = () => {
-    return <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="MainNavigator" component={MainNavigator} />
-        <Tab.Screen name="Camera" component={Camera} />
-        <Tab.Screen name="Bookmark" component={Bookmark} />
-        <Tab.Screen name="Diary" component={Diary} />
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: "red",
+      }}
+    >
+      <Tab.Screen
+        name="MainNavigator"
+        component={MainNavigator}
+        options={{
+          tabBarIcon: (props: TabBarIconProp) => (
+            <FontAwesome
+              name="home"
+              size={26}
+              color={props.focused ? props.color : "gray"}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: (props: TabBarIconProp) => (
+            <FontAwesome
+              name="search"
+              size={23}
+              color={props.focused ? props.color : "gray"}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Camera"
+        component={Camera}
+        options={{
+          tabBarIcon: (props: TabBarIconProp) => (
+            <Entypo
+              name="camera"
+              size={24}
+              color={props.focused ? props.color : "gray"}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Bookmark"
+        component={Bookmark}
+        options={{
+          tabBarIcon: (props: TabBarIconProp) => (
+            <AntDesign
+              name="star"
+              size={24}
+              color={props.focused ? props.color : "gray"}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Diary"
+        component={Diary}
+        options={{
+          tabBarIcon: (props: TabBarIconProp) => (
+            <Entypo
+              name="calendar"
+              size={24}
+              color={props.focused ? props.color : "gray"}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
-}
+  );
+};
 
 export default TabNav;
--- frontend/src/screens/index.tsx
+++ frontend/src/screens/index.tsx
@@ -1,6 +1,7 @@
 import Main from "./main";
+import Search from "./search";
 import Camera from "./camera";
 import Bookmark from "./bookmark";
 import Diary from "./diary";
 
-export { Main, Camera, Bookmark, Diary };
+export { Main, Search, Camera, Bookmark, Diary };
