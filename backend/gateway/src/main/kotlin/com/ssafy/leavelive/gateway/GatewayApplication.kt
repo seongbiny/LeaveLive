ce29b994326d6dc831e8cee5715a1e45a043f15b
sangwoo420 <sangwoo420@gmail.com>
2022-04-26 16:40:39 +0900

    :tada: feat: init gateway project

---
+++ backend/gateway/src/main/kotlin/com/ssafy/leavelive/gateway/GatewayApplication.kt
@@ -0,0 +1,27 @@
+package com.ssafy.leavelive.gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec
+import org.springframework.cloud.gateway.route.builder.PredicateSpec
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+
+@SpringBootApplication
+class GatewayApplication {
+	@Bean
+	fun myRoutes(builder: RouteLocatorBuilder) : RouteLocator {
+		return builder
+			.routes()
+			.route({p:PredicateSpec -> p
+				.path("/get")
+				.filters({f:GatewayFilterSpec -> f.addRequestHeader("Hello", "World")})
+				.uri("http://httpbin.org:80")})
+			.build()
+	}
+}
+
+fun main(args: Array<String>) {
+	runApplication<GatewayApplication>(*args)
+}
