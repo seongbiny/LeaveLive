38eecd20511e3683a6183227628bd95cf0632c9f
김혜인 <fallingstars2749@gmail.com>
2022-04-26 15:53:36 +0900

    :lipstick: [S06P31C105-80] Feat: Add ThemeProvider
    ThemeProvider를 사용해서 네비게이션 색상을 변경했습니다.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from "styled-components/native";
 import Navigation from "./navigations";
+import { StatusBar } from "react-native";
+import { theme } from "./theme";
 
 export default function App() {
   return (
-    <Navigation />
+    <ThemeProvider theme={theme}>
+      <StatusBar backgroundColor={theme.white} barStyle="dark-content" />
+      <Navigation />
+    </ThemeProvider>
   );
 }
--- frontend/src/screens/index.tsx
+++ frontend/src/screens/index.tsx
@@ -1,7 +1,10 @@
-import Main from "./main";
-import Search from "./search";
-import Camera from "./camera";
-import Bookmark from "./bookmark";
-import Diary from "./diary";
+import Main from "./Main/Main";
+import Search from "./Search";
+import Camera from "./Camera";
+import Bookmark from "./Bookmark";
+import Diary from "./Diary";
+
+import Login from "./Login";
 
 export { Main, Search, Camera, Bookmark, Diary };
+export { Login };
