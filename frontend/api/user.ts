8a192fe95ce2714a9ec19487e62e8f1e4df10511
김혜인 <fallingstars2749@gmail.com>
2022-05-03 18:09:53 +0900

    :sparkles: [S06P31C105-139] feat: Kakao Login (complete)
    카카오 소셜 로그인을 구현했어요.

--- frontend/api/user.ts
+++ frontend/api/user.ts
@@ -2,10 +2,10 @@ import { apiInstance } from ".";
 
 const api = apiInstance();
 
-export const GoogleLoginRequest = async (params: string, success: any, fail: any) => {
+export const GoogleLoginRequest = async (params: any, success: any, fail: any) => {
     await api.post(`/auth/google`, params).then(success).catch(fail);
 }
 
 export const KakaoLoginRequest = async (params: any, success: any, fail: any) => {
-    await api.get(`/auth/kakao`, params).then(success).catch(fail);
+    await api.post(`/auth/kakao`, params).then(success).catch(fail);
 }
--- frontend/pages/login/oauth2/redirect.tsx
+++ frontend/pages/login/oauth2/redirect.tsx
@@ -1,49 +1,69 @@
 import React, { useEffect, useCallback } from "react";
 import { useRouter } from "next/router";
 import { KakaoLoginRequest } from "../../../api/user";
+import axios from "axios";
 
 const Redirect = () => {
   const router = useRouter();
 
-  const getCode = useCallback((code: string) => {
-    console.log(code);
-    KakaoLoginRequest(
+  // 회원가입/로그인 성공시 callback 함수
+  const onLoginSuccess = useCallback(
+    // 받아온 jwt token들을 localStorage에 저장
+    ({ data: { access_token, refresh_token } }: any) => {
+      localStorage.setItem("access_token", access_token);
+      localStorage.setItem("refresh_token", refresh_token);
+    },
+    []
+  );
+
+  // kakao 토큰 성공 시 callback 함수
+  const onKakaoGetTokenSuccess = useCallback(
+    ({ data: { access_token } }: any) => {
+      console.log(access_token);
+      const params = {
+        token: access_token,
+      };
+
+      KakaoLoginRequest(params, onLoginSuccess, (error: Error) =>
+        console.log(error)
+      );
+
+      // 메인 화면으로 이동
+      router.push("/");
+    },
+    []
+  );
+
+  // kakao에서 토큰 받아오는 함수
+  const kakaoGetToken = useCallback((code: string) => {
+    const params: any = {
+      grant_type: "authorization_code",
+      client_id: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID,
+      redirect_uri: "http://www.localhost:3000/login/oauth2/redirect",
       code,
-      (response: any) => {
-        console.log(response);
-      },
-      (error: Error) => {
-        console.log(error);
-      }
-    );
+    };
+
+    const queryParams = Object.keys(params)
+      .map((k) => encodeURIComponent(k) + "=" + encodeURI(params[k]))
+      .join("&");
+
+    axios
+      .post(`https://kauth.kakao.com/oauth/token`, queryParams, {
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded",
+        },
+      })
+      .then(onKakaoGetTokenSuccess)
+      .catch((error: Error) => console.log(error));
   }, []);
 
   useEffect(() => {
-    if (router) console.log(router);
-
     if (router.query.code) {
       const code = String(router.query.code);
-      getCode(code);
+      // getCode(code);
+      kakaoGetToken(code);
     }
   }, [router]);
-
-  // useEffect(() => {
-  //   const router = useRouter();
-
-  //   if (router.query.token && router.query.refreshtoken) {
-  //     /** LocalStorage에 token들 저장 */
-  //     const authorization = `Bearer ${router.query.token}`;
-  //     const refreshtoken = `Refresh Bearer ${router.query.refreshToken}`;
-
-  //     localStorage.setItem("authorization", authorization);
-  //     localStorage.setItem("refreshtoken", refreshtoken);
-
-  //     // todo: user 정보를 불러옴
-  //   }
-
-  //   // 메인 화면으로 이동
-  //   router.push("/");
-  // }, []);
 
   return null;
 };
