3b022cbc0fc70e8519bae172fdc285956cec5402
조은지 <dmswl_0311@naver.com>
2022-05-03 14:00:42 +0900

    :hammer: chore: 구글 로그인 로직 변경

--- backend/google_login/jwt/src/main/java/com/security/jwt/controller/JwtCreateController.java
+++ backend/google_login/jwt/src/main/java/com/security/jwt/controller/JwtCreateController.java
@@ -5,52 +5,85 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.security.jwt.domain.User;
 import com.security.jwt.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.http.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.nio.charset.Charset;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.*;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class JwtCreateController {
-    private final UserRepository repo;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final String SECRET_KEY = "blahblahblahblahblahblahblahblahblah";
 
-    @PostMapping("/api/oauth/jwt/google")
-    public List<String> jwtCreate(@RequestBody Map<String,Object> data){
-        List<String> tokens=new ArrayList<>();
-        System.out.println("jwtCreateController 실행");
-        System.out.println("유저 정보 data :: "+data.get("profileObj"));
-        Map<String,Object> map= (Map<String, Object>) data.get("profileObj");
-//        기존에 가입한 회원인지 확인
-        User userEntity =repo.findByUsername("google_"+String.valueOf(map.get("googleId")));
-//        회원 가입 진행
-        if(userEntity==null){
-            User userRequest = new User();
-            userRequest.setUsername("google_"+String.valueOf(map.get("googleId")));
-            userRequest.setRole("ROLE_USER");
-            userRequest.setEmail(String.valueOf(map.get("email")));
-            userRequest.setImg_url(String.valueOf(map.get("imageUrl")));
-            userRequest.setNickname(String.valueOf(map.get("name")));
-            userRequest.setPassword(bCryptPasswordEncoder.encode(UUID.randomUUID().toString())); //임의의 비밀번호
-            userEntity=repo.save(userRequest);
-        }
-//        토큰 발급
+    @PostMapping("/api/auth/google")
+    public List<String> jwtCreate(@RequestBody Map<String, Object> data) {
+        List<String> tokens = new ArrayList<>();
+        log.info("jwtCreateController 실행");
+        log.info("JwtCreateController.jwtCreate.data:" + data.get("profileObj"));
+        Map<String, Object> map = (Map<String, Object>) data.get("profileObj");
+        // 토큰 만들기
+        String userId = String.valueOf(map.get("googleId")) + "GOOGLE";
         String jwtToken = JWT.create()
-                .withSubject(userEntity.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis()+(60000)*1)) //1분
-                .withClaim("id", userEntity.getId())
-                .withClaim("username", userEntity.getUsername())
-                .sign(Algorithm.HMAC512("cos"));
+                .withExpiresAt(new Date(System.currentTimeMillis() + (60000) * 1)) //1분
+                .withClaim("id", userId)
+                .sign(Algorithm.HMAC512(SECRET_KEY));
         String refreshToken = JWT.create()
-                .withExpiresAt(new Date(System.currentTimeMillis()+(60000)*20160)) //2주
-                .withClaim("uuid", UUID.randomUUID().toString()) //중복 방지
-                .sign(Algorithm.HMAC512("cos"));
-        System.out.println("refreshToken :: "+refreshToken);
+//                .withExpiresAt(new Date(System.currentTimeMillis()+(60000)*20160)) //2주
+                .withClaim("id", UUID.randomUUID().toString())
+                .sign(Algorithm.HMAC512(SECRET_KEY));
+        String picPath = String.valueOf(map.get("imageUrl"));
+        String nickname = String.valueOf(map.get("name")) + LocalDateTime.now();
+
+        // 유저 있는지 없는지 확인, 없으면 가입시키고 refreshToken 발급  // 있으면 걍 refreshToken 발급
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(new MediaType("application", "json", Charset.forName("UTF-8"))); //json으로 설정
+        HttpEntity<?> requestMessage = new HttpEntity<>(httpHeaders);
+        //requestMessage 만들기
+        RestTemplate restTemplate = new RestTemplate();
+        String baseUrl = "http://localhost:8083/api/user";
+        String url = baseUrl + "/" + userId;
+        try {
+            //요청하기
+            ResponseEntity<Boolean> responseEntity = restTemplate.getForEntity(url, Boolean.class, requestMessage);
+            log.info("JwtCreateController.jwtCreate.response_body:" + responseEntity.getBody());
+            if (!responseEntity.getBody()) {
+                //존재하지 않음
+                url=baseUrl;
+                httpHeaders.set("Authorization", jwtToken);
+                Map<String, String> body = new HashMap<>();
+                body.put("picPath", picPath);
+                body.put("nickname", nickname);
+                body.put("token", refreshToken);
+                requestMessage = new HttpEntity<>(body, httpHeaders);
+                ResponseEntity<Map> responseMap = restTemplate.postForEntity(url, requestMessage, Map.class);
+                log.info("JwtCreateController.jwtCreate.response_body:" + responseEntity.getBody());
+                if (responseMap.getStatusCode() != HttpStatus.OK) {
+                    throw new RuntimeException("로그인이 되지 않습니다.");
+                }
+            } else {
+                // 존재
+                url = baseUrl + "/refresh-token/" + userId;
+                ResponseEntity<String> refreshTokenResponse = restTemplate.getForEntity(url, String.class, requestMessage);
+                if(refreshTokenResponse.getStatusCode()!=HttpStatus.OK){
+                    throw new RuntimeException("로그인이 되지 않습니다.");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("JwtCreateController.jwtCreate.response:error");
+        }
         tokens.add(jwtToken);
         tokens.add(refreshToken);
         return tokens;
