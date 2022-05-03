2d2c67d2ee614c108d780cb5854e24a1f9634c24
김혜인 <fallingstars2749@gmail.com>
2022-05-03 14:06:59 +0900

    :sparkles: [S06P31C105-141] feat: Split User and Ceo Nav
    Layout.tsx에서 사용자 페이지와 사장님 페이지에 사용할 네비게이션을 각각 분리했어요.

---
+++ frontend/components/CeoNav.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CeoNav = () => {
+  return <div>사장님내비</div>;
+};
+
+export default CeoNav;
--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,21 +1,33 @@
 import SimpleBottomNavigation from "./UserNav1";
-import styled from 'styled-components';
+import styled from "styled-components";
 import useIsMobile from "../util/hooks";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import CeoNav from "./CeoNav";
 
 const Container = styled.div`
-    width: 100%;
+  width: 100%;
 `;
 
-export default function Layout({children}: any){
-    const isMobile = useIsMobile();
-    return(
-        <div>
-            {isMobile ?
-                <>
-                    <div>{children}</div>
-                    <SimpleBottomNavigation />
-                </> : <div>모바일로 접속해주세요.</div>
-            }
-        </div>
-    )
+export default function Layout({ children }: any) {
+  const router = useRouter();
+  const [isUser, setIsUser] = useState<boolean>(true);
+  useEffect(() => {
+    if (router.pathname.startsWith("/ceo") && isUser) setIsUser(false);
+    if (!router.pathname.startsWith("/ceo") && !isUser) setIsUser(true);
+  }, [router]);
+
+  const isMobile = useIsMobile();
+  return (
+    <div>
+      {isMobile ? (
+        <>
+          <div>{children}</div>
+          {isUser ? <SimpleBottomNavigation /> : <CeoNav />}
+        </>
+      ) : (
+        <div>모바일로 접속해주세요.</div>
+      )}
+    </div>
+  );
 }
