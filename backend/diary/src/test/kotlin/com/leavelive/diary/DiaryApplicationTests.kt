5cc5b76ebcfcfc1177fbbea4a756d277f455a649
sangwoo420 <sangwoo420@gmail.com>
2022-05-04 09:56:16 +0900

    :tada: init: diary server

---
+++ backend/diary/src/test/kotlin/com/leavelive/diary/DiaryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.leavelive.diary
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiaryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
