e90f412bf6a40aab6237f5dc3521cee6aeb4699b
seongbiny <s30ngb1n@gmail.com>
2022-05-06 17:28:27 +0900

    :sparkles: [S06P31C105-156] korea map svg 기능 1차 완성

--- frontend/pages/bookmark/index.tsx
+++ frontend/pages/bookmark/index.tsx
@@ -1,12 +1,68 @@
 import React from "react";
 import Seo from "../../components/Seo";
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import List from "../../components/bookmark/list";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 const Bookmark = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
-    <>
+    <Box sx={{ width: '100%', height: '100%', border: '1px solid' }}>
       <Seo title="bookmark" />
-      <div>bookmark</div>
-    </>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs  value={value} onChange={handleChange} variant="fullWidth" aria-label="basic tabs example" centered>
+          <Tab label="숙소" {...a11yProps(0)} />
+          <Tab label="액티비티" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0} >
+        <List />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <div>총 금액</div>
+    </Box>
   )
 };
 
--- frontend/pages/search/[region].tsx
+++ frontend/pages/search/[region].tsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Seo from "../../components/Seo";
+import Image from "next/image";
+import styled from 'styled-components';
+import MainSlider from "../../components/main/MainSlider";
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import NavigationIcon from '@mui/icons-material/Navigation';
+import { useRouter } from 'next/router'
 
-const Login = () => {
-  return <div>테스트</div>;
+const Region = () => {
+  const router = useRouter();
+  
+  return (
+    <>
+      <Seo title="Main" />
+      <StyledBox>
+        <Image src="/main-background.png" alt="main" title="main" width="100%" height="56%" layout="responsive" objectFit="contain"/>
+        <Text>{router.query.region}로 여행 어때요?</Text>
+      </StyledBox>
+      <div style={{marginTop: '2vh', marginBottom: '2vh'}}>이 지역에 살아본 사람들은 하루 평균 50,000원을 썼어요.</div>
+      <Tab>
+        <Box sx={{ '& > :not(style)': { m: 1 }, mb: '2vh' }}>
+          <Fab variant="extended" size="small" color="primary" aria-label="add" >
+            <NavigationIcon sx={{ mr: 1 }} />
+            숙소
+          </Fab>
+          <Fab variant="extended" size="small" color="primary" aria-label="add">
+            <NavigationIcon sx={{ mr: 1 }} />
+            액티비티
+          </Fab>
+        </Box>
+      </Tab>
+      <div>한달 살기 인기 숙소</div>
+      <MainSlider />
+      <div>한달 살기 인기 액티비티</div>
+      <MainSlider />
+    </>
+  )
 };
 
-export default Login;
+const StyledBox = styled.div`
+  position: relative;
+`;
+const Tab = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Text = styled.div`
+  // z-index: 100;
+  position: absolute;
+  color: white;
+  top: 65%;
+  left: 10%;
+`;
+
+export default Region;
