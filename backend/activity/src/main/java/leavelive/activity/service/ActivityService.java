c1784c4341a9756b5cce5efa0c701414d900fe07
조은지 <dmswl_0311@naver.com>
2022-05-09 10:18:00 +0900

    :hammer: chore: 액티비티 이미지 경로 수정

--- backend/activity/src/main/java/leavelive/activity/service/ActivityService.java
+++ backend/activity/src/main/java/leavelive/activity/service/ActivityService.java
@@ -16,9 +16,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -26,129 +24,131 @@ import java.util.Optional;
 public class ActivityService {
     private final ActivityRepo repo;
 
-    public List<ActivityDto> getAllAct(String loc){
-        List<Activity> entities=repo.findAllByLocStartsWith(loc);
-        List<ActivityDto> dtos=new ArrayList<>();
-        ActivityDto dto=new ActivityDto();
-        for(Activity entity:entities){
-            dto=new ActivityDto();
+    public List<ActivityDto> getAllAct(String loc) {
+        List<Activity> entities = repo.findAllByLocStartsWith(loc);
+        List<ActivityDto> dtos = new ArrayList<>();
+        ActivityDto dto = new ActivityDto();
+        for (Activity entity : entities) {
+            dto = new ActivityDto();
             dtos.add(dto.of(entity));
         }
         return dtos;
     }
-    public ActivityDto getAct(Long id){
-        Optional<Activity> entity=repo.findById(id);
-        if(!entity.isPresent()) throw new NullPointerException("해당하는 액티비티가 없습니다.");
-        ActivityDto dto=new ActivityDto();
+
+    public ActivityDto getAct(Long id) {
+        Optional<Activity> entity = repo.findById(id);
+        if (!entity.isPresent()) throw new NullPointerException("해당하는 액티비티가 없습니다.");
+        ActivityDto dto = new ActivityDto();
         return dto.of(entity.get());
     }
-    public Boolean delAct(Long id,String userId){
+
+    public Boolean delAct(Long id, String userId) {
         // userId 있는지 확인
-        Optional<Activity> entity=repo.findById(id);
-        if(!entity.isPresent()) throw new NullPointerException("해당하는 액티비티가 없습니다.");
-        if(!entity.get().getUserId().equals(userId)) throw new NullPointerException("직접 등록한 액티비티만 삭제할 수 있습니다.");
+        Optional<Activity> entity = repo.findById(id);
+        if (!entity.isPresent()) throw new NullPointerException("해당하는 액티비티가 없습니다.");
+        if (!entity.get().getUserId().equals(userId)) throw new NullPointerException("직접 등록한 액티비티만 삭제할 수 있습니다.");
         repo.deleteById(id);
         return true;
     }
-    public ActivityDto saveAct(ActivityDto dto, List<MultipartFile> files, String userId){
-        if(files!=null) dto.setPicPath(saveImage(files));
+
+    public ActivityDto saveAct(ActivityDto dto, List<MultipartFile> files, String userId) {
+        if (files != null) dto.setPicPath(saveImage(files));
         dto.setUserId(userId);
-        Activity activity=new Activity();
-        Activity response=repo.save(activity.of(dto));
+        Activity activity = new Activity();
+        Activity response = repo.save(activity.of(dto));
         return dto.of(response);
     }
-    public ActivityDto updateAct(Long id, ActivityDto dto, String userId){
-        Optional<Activity> entity=repo.findById(id);
-        if(!entity.isPresent()) throw new NullPointerException("해당하는 액티비티가 없습니다.");
-        if(!entity.get().getUserId().equals(userId)) throw new NullPointerException("직접 등록한 액티비티만 삭제할 수 있습니다.");
+
+    public ActivityDto updateAct(Long id, ActivityDto dto, String userId) {
+        Optional<Activity> entity = repo.findById(id);
+        if (!entity.isPresent()) throw new NullPointerException("해당하는 액티비티가 없습니다.");
+        if (!entity.get().getUserId().equals(userId)) throw new NullPointerException("직접 등록한 액티비티만 삭제할 수 있습니다.");
         // 수정 logic
-        log.info("ActivityService.updateAct.dto:"+dto);
-        ActivityDto ori=new ActivityDto();
-        ori=ori.of(entity.get()); // 원래 정보
-        if(dto.getLoc()!=null){
+        log.info("ActivityService.updateAct.dto:" + dto);
+        ActivityDto ori = new ActivityDto();
+        ori = ori.of(entity.get()); // 원래 정보
+        if (dto.getLoc() != null) {
             ori.setLoc(dto.getLoc());
-        }if(dto.getCnt()!=0){
+        }
+        if (dto.getCnt() != 0) {
             ori.setCnt(dto.getCnt());
-        }if(dto.getPrice()!=0){
+        }
+        if (dto.getPrice() != 0) {
             ori.setPrice(dto.getPrice());
-        }if(dto.getContents()!=null){
+        }
+        if (dto.getContents() != null) {
             ori.setContents(dto.getContents());
-        }if(dto.getName()!=null){
+        }
+        if (dto.getName() != null) {
             ori.setName(dto.getName());
         }
-        Activity response=repo.save(entity.get().updateOf(ori));
+        Activity response = repo.save(entity.get().updateOf(ori));
         return dto.of(response);
     }
+
     public byte[] findImage(String imgPath) throws IOException {
         InputStream imageStream;
-        try{
-            imageStream=new FileInputStream(imgPath);
-        }catch(Exception e){
+        try {
+            imageStream = new FileInputStream(imgPath);
+        } catch (Exception e) {
             e.printStackTrace();
             throw new NullPointerException("해당하는 파일이 없습니다.");
         }
-        byte[] imageByteArray= IOUtils.toByteArray(imageStream);
+        byte[] imageByteArray = IOUtils.toByteArray(imageStream);
         imageStream.close();
         return imageByteArray;
     }
-    public String saveImage(List<MultipartFile> files){
-        String picPath="";
-        if(files!=null){
-            LocalDateTime now= LocalDateTime.now(); //현재 시간 저장
+
+    public String saveImage(List<MultipartFile> files) {
+        String picPath = "";
+        String abPath = "/home/ubuntu/images/activity";
+        if (files != null) {
+            LocalDateTime now = LocalDateTime.now(); //현재 시간 저장
             DateTimeFormatter dateTimeFormatter =
                     DateTimeFormatter.ofPattern("yyyyMMdd"); //시간 포맷 바꾸기
             String current_date = now.format(dateTimeFormatter);
 
-            String abPath=new File("").getAbsolutePath()+ File.separator; //절대 경로
-            log.info("AccommodationServiceImpl.saveImage.abPath:"+abPath);
-
-            // 세부 경로
-            String path="images"+File.separator+current_date;
-            File file=new File(path);
-            log.info("AccommodationServiceImpl.saveImage.path:"+path);
-
+            File file = new File(abPath);
             // 폴더가 존재하지 않으면
-            if(!file.exists()){
-                boolean success=file.mkdir();
-                if(!success){
+            if (!file.exists()) {
+                boolean success = file.mkdir();
+                if (!success) {
                     throw new NullPointerException("파일 경로를 생성하지 못했습니다.");
                 }
             }
-            for (MultipartFile multipartFile:files){
+            for (MultipartFile multipartFile : files) {
                 String originalFileExtension;
-                String contentType=multipartFile.getContentType();
-                if(ObjectUtils.isEmpty(contentType)){
+                String contentType = multipartFile.getContentType();
+                if (ObjectUtils.isEmpty(contentType)) {
                     continue;
                 }
-                if(contentType.contains("image/PNG")){
-                    originalFileExtension=".PNG";
-                }else if(contentType.contains("image/png")){
-                    originalFileExtension=".png";
-                }else if(contentType.contains("image/jpeg")){
-                    originalFileExtension=".jpeg";
-                }else if(contentType.contains("image/JPEG")){
-                    originalFileExtension=".JPEG";
-                }else{
+                if (contentType.toLowerCase(Locale.ROOT).contains("image/png")) {
+                    originalFileExtension = ".png";
+                } else if (contentType.toLowerCase(Locale.ROOT).contains("image/jpeg")) {
+                    originalFileExtension = ".jpeg";
+                } else if (contentType.toLowerCase(Locale.ROOT).contains("image/jpg")) {
+                    originalFileExtension = ".jpg";
+                } else {
                     log.error("AccommodationServiceImpl.saveImage:이미지 파일만 올릴 수 있습니다.");
                     continue;
                 }
-                String new_file_name=System.nanoTime()+multipartFile.getName()+originalFileExtension;
+                String new_file_name = current_date + UUID.randomUUID() + originalFileExtension;
 
                 // 업로드 한 파일 데이터를 지정한 파일에 저장
-                try{
-                    file = new File(abPath + path + File.separator + new_file_name);
+                try {
+                    file = new File(abPath + File.separator + new_file_name);
                     multipartFile.transferTo(file);
-                }catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                     throw new NullPointerException("이미지 저장에 실패했습니다.");
                 }
                 file.setWritable(true);
                 file.setReadable(true);
 
-                picPath+=abPath + path + File.separator + new_file_name+",";
+                picPath += "activity" + File.separator + new_file_name + ",";
             }
             // 마지막 콤마는 빼기
-            picPath=picPath.substring(0,picPath.length()-1);
+            picPath = picPath.substring(0, picPath.length() - 1);
         }
         return picPath;
     }
