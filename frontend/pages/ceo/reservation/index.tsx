3f572046f226b33a93e403133a27008c3450562a
김혜인 <fallingstars2749@gmail.com>
2022-05-11 11:01:12 +0900

    :sparkles: [S06P31C105-147] feat: 사장님 예약 목록 api

--- frontend/pages/ceo/reservation/index.tsx
+++ frontend/pages/ceo/reservation/index.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getMyReservationList } from "../../../api/ceo";
 
-const Login = () => {
+interface IReservation {
+  userName: string;
+  cnt: number;
+  bnbName: string;
+  startDate: string;
+  endDate: string;
+  price: number;
+}
+
+const MyReservation = () => {
+  useEffect(() => {
+    getMyReservationList(
+      null,
+      ({ data }: any) => {
+        const reservationList: Array<IReservation> = [];
+        data.forEach((d: any) => {
+          const reservation: IReservation = {
+            userName: d.userId,
+            cnt: d.cnt,
+            bnbName: d.accommodationArticle.name,
+            startDate: d.startDate,
+            endDate: d.endDate,
+            price: d.accommodationArticle.price * d.cnt,
+          };
+
+          reservationList.push(reservation);
+        });
+
+        setReservations(reservationList);
+      },
+      (error: Error) => console.log(error)
+    );
+  }, []);
+
+  const [reservations, setReservations] = useState<Array<IReservation>>();
   return <div>테스트</div>;
 };
 
-export default Login;
+export default MyReservation;
