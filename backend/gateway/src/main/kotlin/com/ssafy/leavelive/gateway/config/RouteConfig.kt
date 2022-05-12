9fa604c75be78ceb497e043ad5c7465e50657882
sangwoo420 <sangwoo420@gmail.com>
2022-05-12 15:49:25 +0900

    :hammer: update: route path

--- backend/gateway/src/main/kotlin/com/ssafy/leavelive/gateway/config/RouteConfig.kt
+++ backend/gateway/src/main/kotlin/com/ssafy/leavelive/gateway/config/RouteConfig.kt
@@ -17,53 +17,53 @@ class RouteConfig(private val authenticationFilter: AuthenticationFilter) {
             .route("kakao_auth_route") {
                 it
                     .path("/api/auth/kakao/**")
-                    .uri("http://localhost:8081")
+                    .uri("https://localhost:8081")
             }
             .route("google_auth_route") {
                 it
                     .path("/api/auth/google/**")
-                    .uri("http://localhost:8082")
+                    .uri("https://localhost:8082")
             }
             .route("token_republish_route") {
                 it.path("/api/auth/token/**")
-                    .uri("http://localhost:8081")
+                    .uri("https://localhost:8081")
             }
             .route("user_route") {
                 it
                     .path("/api/user/**")
                     .filters { f: GatewayFilterSpec -> f.filter(authenticationFilter) }
-                    .uri("http://localhost:8083")
+                    .uri("https://localhost:8083")
             }
             .route("accommodation_route") {
                 it
                     .path("/api/accommodation/**")
                     .filters { f: GatewayFilterSpec -> f.filter(authenticationFilter) }
-                    .uri("http://localhost:8084")
+                    .uri("https://localhost:8084")
 
             }
             .route("accommodation_exclude_auth") {
                 it
                     .order(-1)
                     .path("/api/accommodation/all/**", "/api/accommodation/detail/**")
-                    .uri("http://localhost:8084")
+                    .uri("https://localhost:8084")
             }
             .route("activity_route") {
                 it
                     .path("/api/activity/**")
                     .filters { f: GatewayFilterSpec -> f.filter(authenticationFilter) }
-                    .uri("http://localhost:8085")
+                    .uri("https://localhost:8085")
             }
             .route("activity_exclude_path") {
                 it
                     .order(-1)
                     .path("/api/activity/all/**", "/api/activity/detail/**")
-                    .uri("http://localhost:8085")
+                    .uri("https://localhost:8085")
             }
             .route("diary_route") {
                 it
                     .path("/api/diary/**")
                     .filters { f: GatewayFilterSpec -> f.filter(authenticationFilter) }
-                    .uri("http://localhost:8086")
+                    .uri("https://localhost:8086")
             }
 
             .build()
