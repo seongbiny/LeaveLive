fc31b505666b3851a21b21795fdf91fc42c8d800
sangwoo420 <sangwoo420@gmail.com>
2022-05-12 15:55:02 +0900

    :hammer: update: define url implicitly

--- backend/gateway/src/main/kotlin/com/ssafy/leavelive/gateway/config/RouteConfig.kt
+++ backend/gateway/src/main/kotlin/com/ssafy/leavelive/gateway/config/RouteConfig.kt
@@ -7,6 +7,8 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+private const val URL = "k6c105.p.ssafy.io"
+
 @Configuration
 class RouteConfig(private val authenticationFilter: AuthenticationFilter) {
 
@@ -17,53 +19,53 @@ class RouteConfig(private val authenticationFilter: AuthenticationFilter) {
             .route("kakao_auth_route") {
                 it
                     .path("/api/auth/kakao/**")
-                    .uri("https://localhost:8081")
+                    .uri("https://$URL:8081")
             }
             .route("google_auth_route") {
                 it
                     .path("/api/auth/google/**")
-                    .uri("https://localhost:8082")
+                    .uri("https://$URL:8082")
             }
             .route("token_republish_route") {
                 it.path("/api/auth/token/**")
-                    .uri("https://localhost:8081")
+                    .uri("https://$URL:8081")
             }
             .route("user_route") {
                 it
                     .path("/api/user/**")
                     .filters { f: GatewayFilterSpec -> f.filter(authenticationFilter) }
-                    .uri("https://localhost:8083")
+                    .uri("https://$URL:8083")
             }
             .route("accommodation_route") {
                 it
                     .path("/api/accommodation/**")
                     .filters { f: GatewayFilterSpec -> f.filter(authenticationFilter) }
-                    .uri("https://localhost:8084")
+                    .uri("https://$URL:8084")
 
             }
             .route("accommodation_exclude_auth") {
                 it
                     .order(-1)
                     .path("/api/accommodation/all/**", "/api/accommodation/detail/**")
-                    .uri("https://localhost:8084")
+                    .uri("https://$URL:8084")
             }
             .route("activity_route") {
                 it
                     .path("/api/activity/**")
                     .filters { f: GatewayFilterSpec -> f.filter(authenticationFilter) }
-                    .uri("https://localhost:8085")
+                    .uri("https://$URL:8085")
             }
             .route("activity_exclude_path") {
                 it
                     .order(-1)
                     .path("/api/activity/all/**", "/api/activity/detail/**")
-                    .uri("https://localhost:8085")
+                    .uri("https://$URL:8085")
             }
             .route("diary_route") {
                 it
                     .path("/api/diary/**")
                     .filters { f: GatewayFilterSpec -> f.filter(authenticationFilter) }
-                    .uri("https://localhost:8086")
+                    .uri("https://$URL:8086")
             }
 
             .build()
