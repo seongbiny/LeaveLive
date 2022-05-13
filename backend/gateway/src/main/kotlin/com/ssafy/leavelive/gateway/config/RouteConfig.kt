8f228326c81e66650167a3898c6bd456bca260c1
박상우 <sangwoo420@gmail.com>
2022-05-13 01:50:43 +0000

    Update backend/gateway/src/main/kotlin/com/ssafy/leavelive/gateway/config/RouteConfig.kt

--- backend/gateway/src/main/kotlin/com/ssafy/leavelive/gateway/config/RouteConfig.kt
+++ backend/gateway/src/main/kotlin/com/ssafy/leavelive/gateway/config/RouteConfig.kt
@@ -7,7 +7,7 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-private const val URL = "k6c105.p.ssafy.io"
+private const val URL = "localhost"
 
 @Configuration
 class RouteConfig(private val authenticationFilter: AuthenticationFilter) {
