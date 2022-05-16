78acb7218768a04a64828b762367f3cbb7510b45
seongbiny <s30ngb1n@gmail.com>
2022-05-17 03:16:34 +0900

    :sparkles: [S06P31C105-215] feat: share page layout and rest api

--- frontend/api/diary.ts
+++ frontend/api/diary.ts
@@ -9,3 +9,7 @@ export const getMyAllDiary = async (params: any, success: any, fail: any) => {
 export const getDiary = async (date: any, success: any, fail: any) => {
   await api.get(`/diary?date=${date}`).then(success).catch(fail);
 };
+
+export const publicDiary = async (params: any, success: any, fail: any) => {
+  await api.get(`/diary/public`).then(success).catch(fail);
+};
