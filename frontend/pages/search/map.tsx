34d4b173be1ff4c8d535f80f20c2d86e84032a79
seongbiny <s30ngb1n@gmail.com>
2022-05-16 10:49:34 +0900

    :lipstick: [S06P31C105-201] style: map page css update

--- frontend/pages/search/map.tsx
+++ frontend/pages/search/map.tsx
@@ -10,23 +10,13 @@ const Container = styled.div`
   margin-top: 5vh;
   // margin-left: 5vw;
 `
-const StyledMap = styled(Map)`
-  &:hover{
-    path {
-      fill: red;
-    }
-  }
-  path {
-    fill: blue;
-  }
-`;
 
 const KoreaMap = () => {
   const router = useRouter();
   const region = ['부산', '대구', '대전', '강원', '광주', '경기', '인천', '제주', '충청북도', '경상북도', '전라북도', '세종', '서울', '충청남도', '경상남도', '전라남도', '울산']
   return (
     <Container>
-      <div style={{marginBottom: '7vh', fontSize: '25px', textAlign: 'center'}}>우리나라</div>
+      <div style={{marginBottom: '5vh', fontSize: '25px', textAlign: 'center'}}>어디로 갈까요?</div>
       <Map width="100%" height="100%" fill={"lightgreen"} stroke={"lightgreen"} fillOpacity={0.5} />
     </Container>
   )
