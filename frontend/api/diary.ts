857ec4bffb8fb5473eaaec296e001d1ca080dfb1
seongbiny <s30ngb1n@gmail.com>
2022-05-16 20:51:52 +0900

    :bug: fix: diary rest api fix

--- frontend/api/diary.ts
+++ frontend/api/diary.ts
@@ -5,3 +5,7 @@ const api = apiInstance();
 export const getMyAllDiary = async (params: any, success: any, fail: any) => {
   await api.get(`/diary/my-diary`).then(success).catch(fail);
 };
+
+export const getDiary = async (date: any, success: any, fail: any) => {
+  await api.get(`/diary?date=${date}`).then(success).catch(fail);
+};
