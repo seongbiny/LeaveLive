ef6ab3e31fcd22978c933f381f8e2741fbeb7272
김혜인 <fallingstars2749@gmail.com>
2022-05-20 00:56:04 +0900

    :ambulance: hotfix: 액티비티 하단 네비게이션 바 CSS

--- frontend/pages/reservation/[region]/activity/[id].tsx
+++ frontend/pages/reservation/[region]/activity/[id].tsx
@@ -1,17 +1,24 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { activityDetail, likeActivityList, likeActivity, unlikeActivity } from "../../../../api/activity";
+import {
+  activityDetail,
+  likeActivityList,
+  likeActivity,
+  unlikeActivity,
+} from "../../../../api/activity";
 import styled from "styled-components";
 import { Carousel } from "react-responsive-carousel";
 import { BACKEND_IMAGE_URL } from "../../../../api";
 import Map from "../../../../components/ceo/BnbMap";
 import { flexCenter } from "../../../../styles/Basic";
 import Header from "../../../../components/Header";
-import AddLocationIcon from '@mui/icons-material/AddLocation';
+import AddLocationIcon from "@mui/icons-material/AddLocation";
 import { Button } from "@mui/material";
 import MyCarousel from "../../../../components/Carousel";
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import DetailNav from "../../../../components/ceo/DetailNav";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+
 interface IDetail {
   cnt: number;
   contents: string;
@@ -66,57 +73,73 @@ const ActivityDetail = () => {
   });
 
   const likeAxios = () => {
-    likeActivity(id,
+    likeActivity(
+      id,
       (response: any) => console.log(response),
-      (error: Error) => console.log(error))
-  }
+      (error: Error) => console.log(error)
+    );
+  };
 
   const unlikeAxios = () => {
-    unlikeActivity(id,
-      (response: any) => (console.log(response)),
-      (error: Error) => console.log(error))
-  }
+    unlikeActivity(
+      id,
+      (response: any) => console.log(response),
+      (error: Error) => console.log(error)
+    );
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     activityDetail(
       id,
-      ({ data }: any) => {setDetail(data)},
+      ({ data }: any) => {
+        setDetail(data);
+      },
       (error: Error) => console.log(error)
-    )
+    );
     likeActivityList(
       null,
-      ({ data }: any) => {setList(data)},
+      ({ data }: any) => {
+        setList(data);
+      },
       (error: Error) => console.log(error)
-    )
-  },[router])
+    );
+  }, [router]);
 
-  useEffect(()=>{
-    console.log(list)
-    list.map((item: any)=>{
-      if(Number(item.activity.id) === Number(id)){
-        setLike(true)
+  useEffect(() => {
+    console.log(list);
+    list.map((item: any) => {
+      if (Number(item.activity.id) === Number(id)) {
+        setLike(true);
       }
-    })
-  },[list])
+    });
+  }, [list]);
 
   return (
-    detail.id !== 0 &&
+    detail.id !== 0 && (
       <Container>
         <div style={{ position: "relative", width: "100%" }}>
           <Header title="상세보기" hide={false} />
           {/* <MyCarousel picPath={detail.picPath} /> */}
           <Text>
-            {like === false ? 
-              <FavoriteBorderIcon 
-                fontSize="medium" 
-                sx={{color: '#FF385C'}} 
-                onClick={() => {setLike(!like); likeAxios();}}
-              /> : 
-              <FavoriteIcon 
-                fontSize="medium" 
-                sx={{color: '#FF385C'}} 
-                onClick={() => {setLike(!like); unlikeAxios();}}
-              />}
+            {like === false ? (
+              <FavoriteBorderIcon
+                fontSize="medium"
+                sx={{ color: "#FF385C" }}
+                onClick={() => {
+                  setLike(!like);
+                  likeAxios();
+                }}
+              />
+            ) : (
+              <FavoriteIcon
+                fontSize="medium"
+                sx={{ color: "#FF385C" }}
+                onClick={() => {
+                  setLike(!like);
+                  unlikeAxios();
+                }}
+              />
+            )}
           </Text>
           {/* <Carousel infiniteLoop showThumbs={false} showStatus={false} showArrows={false} >
             {detail.picPath.split(",").map((pic, idx)=>(
@@ -133,36 +156,37 @@ const ActivityDetail = () => {
           <MyCarousel picPath={detail.picPath} />
         </div>
         <ContentContainer>
-          <div style={{fontSize:'1.5rem', fontWeight:'bold', paddingBottom: '3vh'}}>{detail.name}</div>
+          <div
+            style={{
+              fontSize: "1.5rem",
+              fontWeight: "bold",
+              paddingBottom: "3vh",
+            }}
+          >
+            {detail.name}
+          </div>
           <div>
             <AddLocationIcon />
-            <div style={{fontSize: '1rem', display: 'inline'}}>{detail.loc}</div>
-            <Map
-              address={detail.loc}
-              style={{ margin: "1rem 0"}}
-            />
+            <div style={{ fontSize: "1rem", display: "inline" }}>
+              {detail.loc}
+            </div>
+            <Map address={detail.loc} style={{ margin: "1rem 0" }} />
           </div>
-          <img src={`${BACKEND_IMAGE_URL}/${detail.picContents}`} width="100%" />
+          <img
+            src={`${BACKEND_IMAGE_URL}/${detail.picContents}`}
+            width="100%"
+          />
         </ContentContainer>
-        <Bottom onClick={()=>{router.push(`/reservation/activity/${detail.id}`)}}>
-          <div style={{marginRight:'20vw', fontWeight:'bold', fontSize:'1.2rem'}}>{detail.price}원 / 인</div>
-          <div>
-            <Button variant="contained" >예약하기</Button>
-          </div>
-        </Bottom>
+        <DetailNav
+          price={detail.price}
+          text="예약하기"
+          onClick={() => {
+            router.push(`/reservation/activity/${detail.id}`);
+          }}
+        />
       </Container>
-  )
+    )
+  );
 };
-const Bottom = styled.div`
-  width: 100%;
-  height: 10vh;
-  // border: 1px solid;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: #d3d3d3;
-  position: fixed;
-  margin-top: 90vh;
-  z-index: 100;
-`;
+
 export default ActivityDetail;
